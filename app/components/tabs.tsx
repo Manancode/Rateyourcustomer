a2f2919c6721cf748c0c7233da31776b9d7832de
manancode <mananarorawork@gmail.com>
2024-07-13 04:50:41 +0530

    fixed ssr error

--- app/components/tabs.tsx
+++ app/components/tabs.tsx
@@ -1,58 +1,43 @@
-"use client";
+"use client"
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
 
-import React from 'react';
-import { Tabs, Tab, Box, Typography } from '@mui/material';
-
-function TabPanel(props: { [x: string]: any; children: any; value: any; index: any; }) {
-    const { children, value, index, ...other } = props;
-
-    return (
-        <div
-            role="tabpanel"
-            hidden={value !== index}
-            id={`simple-tabpanel-${index}`}
-            aria-labelledby={`simple-tab-${index}`}
-            {...other}
-        >
-            {value === index && (
-                <Box p={3}>
-                    <Typography>{children}</Typography>
-                </Box>
-            )}
-        </div>
-    );
+export interface TabInfo {
+  label: string;
+  content: React.ReactNode;
 }
 
-function a11yProps(index: number) {
-    return {
-        id: `simple-tab-${index}`,
-        'aria-controls': `simple-tabpanel-${index}`,
-    };
+interface LabTabsProps {
+  tabs: TabInfo[];
 }
 
-interface SimpleTabsProps {
-    tabs: { label: string, content: React.ReactNode }[];
-}
+export default function LabTabs({ tabs }: LabTabsProps) {
+  const [value, setValue] = React.useState('0');
 
-export default function SimpleTabs({ tabs }: SimpleTabsProps) {
-    const [value, setValue] = React.useState(0);
+  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+    setValue(newValue);
+  };
 
-    const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
-        setValue(newValue);
-    };
-
-    return (
-        <div>
-            <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
-                {tabs.map((tab, index) => (
-                    <Tab label={tab.label} {...a11yProps(index)} key={index} />
-                ))}
-            </Tabs>
+  return (
+    <Box sx={{ width: '100%', typography: 'body1' }}>
+      <TabContext value={value}>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <TabList onChange={handleChange} aria-label="lab API tabs example">
             {tabs.map((tab, index) => (
-                <TabPanel value={value} index={index} key={index}>
-                    {tab.content}
-                </TabPanel>
+              <Tab key={index} label={tab.label} value={index.toString()} />
             ))}
-        </div>
-    );
+          </TabList>
+        </Box>
+        {tabs.map((tab, index) => (
+          <TabPanel key={index} value={index.toString()}>
+            {tab.content}
+          </TabPanel>
+        ))}
+      </TabContext>
+    </Box>
+  );
 }
